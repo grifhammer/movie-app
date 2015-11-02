36833457edc8abad0917796f296b2965518bd97e
Griffin Hammer <grifhammer@gmail.com>
2015-11-02 14:55:30 -0500

    First Revision
    Script is written to loop through the movie object array and replace
    current image sources with image sources from the objects in the array.

---
+++ scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	var posters = $('.poster-wrapper')
+	var movieObjIndex = 0;
+	$(posters).each(function(){
+		var currPoster = $(this).find('img')
+		$(currPoster).attr('src', objectArray[movieObjIndex].poster_path);
+		movieObjIndex++;
+	});
+});
