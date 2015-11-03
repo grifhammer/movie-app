29479642efefbf6bd6709d286fbbd67d0a6b42bc
Griffin Hammer <grifhammer@gmail.com>
2015-11-03 14:29:58 -0500

    Broken With Select Box
    A broken search with the select box added

--- scripts.js
+++ scripts.js
@@ -46,27 +46,75 @@ $(document).ready(function(){
 		});
 	 });
 
-	$('#search-button').click(function(){
+	$('#search-form-wrapper').submit(function(){
 		html = '';
 		var userSearch = $('#user-search').val();
-		var searchURL = 'http://api.themoviedb.org/3/search/movie?query=' +userSearch + '&api_key='+ apiKey;
+		var selectBoxVar = $('#search-select').val();
+
+		var searchURL = 'http://api.themoviedb.org/3/search/'+ selectBoxVar + '?query=' +userSearch + '&api_key='+ apiKey;
 		$.getJSON(searchURL, function(data){
 			movieObjIndex = 0;
+			var numPictures = 0;
 			var objectArray = data.results;
-			$(objectArray).each(function(){
-				if(movieObjIndex != 0 && movieObjIndex%4 == 0){
-					html += '</div>'
+
+			var actorFunction = function(){
+				
+				if(objectArray[movieObjIndex].poster_path){
+					if(numPictures != 0 && numPictures%4 == 0){
+						html += '</div>'
+					}
+					if(numPictures % 4 == 0){
+						html += '<div class="row-wrapper">'
+					}
+					posterSource = basePath + 'w300' + objectArray[movieObjIndex++].poster_path;
+					html += '<div class="poster-wrapper">'
+					html += '<img class="poster" src="' + posterSource +'">' + objectArray[movieObjIndex-1].title;
+					html +='</div>'
+					numPictures++;
+				} else{
+					movieObjIndex++;
 				}
-				if(movieObjIndex % 4 == 0){
-					html += '<div class="row-wrapper">'
+			}
+			var movieFunction = function(){
+
+				if(objectArray[movieObjIndex].poster_path){
+					if(numPictures != 0 && numPictures%4 == 0){
+						html += '</div>'
+					}
+					if(numPictures % 4 == 0){
+						html += '<div class="row-wrapper">'
+					}
+					posterSource = basePath + 'w300' + objectArray[movieObjIndex++].poster_path;
+					html += '<div class="poster-wrapper">'
+					html += '<img class="poster" src="' + posterSource +'">' + objectArray[movieObjIndex-1].title;
+					html +='</div>'
+					numPictures++;
+				} else{
+					movieObjIndex++;
 				}
-				posterSource = basePath + 'w300' + objectArray[movieObjIndex++].poster_path;
-				html += '<div class="poster-wrapper">'
-				html += '<img class="poster" src="' + posterSource +'">';
-				html +='</div>'
-			});
+			}
+
+
+			var directorFunction = function(){}
+			var anyFunction = function(){}
+			switch(selectBoxVar){
+				case 'movie':
+					$(objectArray).each(movieFunction());
+					break;
+				case 'actor':
+					$(objectArray).each(actorFunction());
+					break;
+				case 'director':
+					$(objectArray).each(directorFunction());
+					break;
+				case 'multi':
+					$(objectArray).each(directorFunction());
+					break;
+			}
+			$(objectArray).each(movieFunction());
 			$('#searched-movies').html(html);
 		})
+		event.preventDefault();
 	});
 
 
